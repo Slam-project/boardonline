fafc3cf9b3f51dc9e91b53f7d4b95ccd2ed124e5
smile59 <17crazysak59@gmail.com>
2015-06-23 17:24:00 +0200

    Initial commit Release/0.1

---
+++ BoardOnline/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  BoardOnline
+//
+//  Created by Alexandre on 23/06/2015.
+//  Copyright (c) 2015 Les_Smileyz. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
